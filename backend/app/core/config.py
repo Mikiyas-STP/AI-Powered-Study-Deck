c33dcb1d21cf739244ec14283019ec4860d5909e
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-04-28 13:18:30 +0100

    feat(core): implemented pydantic setting management to configure postgres connection , add cors origin list to support react server and setup JWT configuration variables.

---
+++ backend/app/core/config.py
@@ -0,0 +1,31 @@
+#enviromental configurations with pydantic settings management
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "PAPR Portfolio API"
+    API_V1_STR: str = "/api/v1"
+    
+    # CORS Configuration for React
+    # In production, this should be restricted to the  actual frontend domain
+    BACKEND_CORS_ORIGINS: List[str] =["http://localhost:3000", "http://localhost:5173"]
+    
+    # Database
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: str = "5432"
+    POSTGRES_DB: str = "portfolio_db"
+    
+    # JWT
+    SECRET_KEY: str = "CHANGE_THIS_IN_PRODUCTION_TO_A_SECURE_RANDOM_STRING"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
+settings = Settings()

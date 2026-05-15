ff2d2f8a998a5b01e69e92d44d0d5da31b8128ac
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-05-15 17:46:47 +0100

    feat(core): implement global exception handling and logging
    - Created custom handlers for SQLAlchemy and general Python exceptions
    - Ensured consistent JSON error responses for React frontend
    - Added basic logging for server-side error tracking
    - Registered handlers in FastAPI application factory

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,9 @@ from app.api.flashcards import router as flashcard_router
 #import my auth runner from my register/login endpoint file(auth.py)
 from app.api.auth import router as auth_router
 
+from sqlalchemy.exc import SQLAlchemyError
+from app.core.exceptions import sqlalchemy_exception_handler, general_exception_handler
+
 
 def get_application() -> FastAPI:
     """
@@ -33,6 +36,8 @@ def get_application() -> FastAPI:
     app.include_router(auth_router, prefix=f"{settings.API_V1_STR}/auth", tags=["Authentication"])
     app.include_router(deck_router, prefix=f"{settings.API_V1_STR}/decks", tags=["Decks"])
     app.include_router(flashcard_router, prefix=f"{settings.API_V1_STR}/flashcards", tags=["Flashcards"])
+    app.add_exception_handler(SQLAlchemyError, sqlalchemy_exception_handler)
+    app.add_exception_handler(Exception, general_exception_handler)
     return app
 
 app = get_application()

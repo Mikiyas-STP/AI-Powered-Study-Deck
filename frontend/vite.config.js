c72373c33cf87869fced8e440c1e04968369bd77
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-05-17 15:11:08 +0100

    fix(frontend): allow all hosts in vite config for deployment" git push origin main

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    // This is the fix for the "Blocked request" error
+    allowedHosts: true, 
+    host: true, // Ensures Vite listens on all local IPs inside the container
+    port: 5173,
+  }
+
 })
